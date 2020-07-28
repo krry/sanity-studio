a3719209c06f33569a262a0209c9f0a39e14dd64
Kerry Snyder <krry@me.com>
2020-07-28 14:56:56 -0700

    initial commit of studio and web and lerna

---
+++ schemas/schema.js
@@ -0,0 +1,85 @@
+// First, we must import the schema creator
+import createSchema from 'part:@sanity/base/schema-creator'
+
+// Then import schema types from any plugins that might expose them
+import schemaTypes from 'all:part:@sanity/base/schema-type'
+
+// Then we give our schema to the builder and provide the result to Sanity
+export default createSchema({
+  // We name our schema
+  name: 'default',
+  // Then proceed to concatenate our document type
+  // to the ones provided by any plugins that are installed
+  types: schemaTypes.concat([
+    /* Your types here! */
+    {
+      name: 'author',
+      type: 'document',
+      title: 'Author',
+      fields: [
+        {
+          name: 'name',
+          title: 'Name',
+          required: true,
+          type: 'string',
+        },
+        {
+          name: 'picture',
+          title: 'Picture',
+          type: 'image',
+        },
+      ],
+    },
+
+    {
+      name: 'post',
+      type: 'document',
+      title: 'Post',
+      fields: [
+        {
+          name: 'title',
+          title: 'Title',
+          required: true,
+          type: 'string',
+        },
+        {
+          name: 'content',
+          title: 'Content',
+          required: true,
+          type: 'array',
+          of: [{ type: 'block' }],
+        },
+        {
+          name: 'excerpt',
+          title: 'Excerpt',
+          type: 'string',
+        },
+        {
+          name: 'coverImage',
+          title: 'Cover Image',
+          type: 'image',
+        },
+        {
+          name: 'date',
+          title: 'Date',
+          required: true,
+          type: 'datetime',
+        },
+        {
+          name: 'author',
+          title: 'Author',
+          required: true,
+          description: 'Name thyself',
+          type: 'reference',
+          to: [{ type: 'author' }],
+        },
+        {
+          name: 'slug',
+          title: 'Slug',
+          description: 'https://www.atmanautica.com/slug',
+          type: 'slug',
+        },
+      ],
+    },
+  ]),
+})
