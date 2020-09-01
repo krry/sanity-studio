3a9cdc80be85304040ebd871ca234c1622037217
Kerry Snyder <krry@me.com>
2020-08-31 18:56:40 -0700

    transition from full smile

--- brand/fsLogo.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import styles from './fsLogo.module.css'
-
-const fsLogo = () => (
-  <img className={styles.fsLogo} src="/static/full-smile.png" alt="Full Smile" />
-)
-
-export default fsLogo
